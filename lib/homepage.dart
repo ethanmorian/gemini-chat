d8479fdc42c46feec2ea01048a877139bc601e19
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-20 21:03:59 +0900

    feat(homepage): add skeleton loading for 'Now Playing' section

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_website/skeleton_loading/carousel_skeleton.dart';
+import 'package:movie_website/skeleton_loading/now_skeleton.dart';
 import 'package:movie_website/widget/icon_searchbar.dart';
 import 'package:movie_website/widget/main_drawer.dart';
 
@@ -30,7 +31,44 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            CarouselSkeleton(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: CarouselSkeleton(),
+                  ),
+                  flex: 2,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8),
+                        child: Text(
+                          'Now Playing',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      NowSkeleton(),
+                    ],
+                  ),
+                  flex: 1,
+                ),
+              ],
+            ),
           ],
         ),
       ),
