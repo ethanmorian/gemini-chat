79045ac9345193607e4acaac93670ff1e4d0400c
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-10-20 16:05:30 +0900

    feat(app): create initial web app with homepage, custom search bar, and navigation drawer

---
+++ lib/widget/main_drawer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+              ),
+              child: Text('Main'),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+              ),
+              child: Text('Sorting movies'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
